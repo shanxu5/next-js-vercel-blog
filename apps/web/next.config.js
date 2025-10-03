034046afbe0d1092c953e4da802cba8f20fdd7cb
Shan Xu <sxu@overstock.com>
2025-10-03 12:03:15 -0600

    adding basepath in blog; add rewrite in web

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  rewrites() {
+    return [
+      {
+        source: '/blog/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BLOG_URL}/blog/:path*`,
+      },
+    ];
+  },
   images: {
     domains: ['picsum.photos'],
   },
